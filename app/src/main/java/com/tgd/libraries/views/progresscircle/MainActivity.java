545c7b12da6c72406fc788dbb18d824779266afa
Tomer <tomergoldstgithub@gmail.com>
2016-06-17 16:27:27 +0300

    remove button from layout

--- app/src/main/java/com/tgd/libraries/views/progresscircle/MainActivity.java
+++ app/src/main/java/com/tgd/libraries/views/progresscircle/MainActivity.java
@@ -1,23 +1,21 @@
 package com.tgd.libraries.views.progresscircle;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ProgressCircle mCircleProgressIndicator;
+    private ProgressCircle mCircleProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mCircleProgressIndicator = (ProgressCircle) findViewById(R.id.circle_progress_indicator);
+        mCircleProgress = (ProgressCircle) findViewById(R.id.circle_progress);
 
         final Random random = new Random();
 
@@ -26,26 +24,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 int randomProgress = random.nextInt(100);
-                mCircleProgressIndicator.setProgress(randomProgress);
+                mCircleProgress.setProgress(randomProgress);
                 progressHandler.postDelayed(this, 1500);
             }
         };
         progressHandler.post(progressRunnable);
-
-
-        Button setRandomProgressBtn = (Button) findViewById(R.id.button);
-        assert setRandomProgressBtn != null;
-        setRandomProgressBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (progressHandler != null ){
-                    progressHandler.removeCallbacksAndMessages(null);
-                }
-
-                int randomProgress = random.nextInt(100);
-                mCircleProgressIndicator.setProgress(randomProgress);
-            }
-        });
 
     }
 
